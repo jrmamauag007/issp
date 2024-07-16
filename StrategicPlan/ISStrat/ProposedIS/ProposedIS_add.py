967e41883a68d7dd149b31cc6c8ff6c10be70c20
John Robert Mamauag <johnrobert.mamauag@g.msuiit.edu.ph>
2024-07-16 15:15:01 +0800

    Feat: Finish ProposedIS test cases

--- StrategicPlan/ISStrat/ProposedIS/ProposedIS_add.py
+++ StrategicPlan/ISStrat/ProposedIS/ProposedIS_add.py
@@ -106,9 +106,13 @@ def ProposedIS_add(driver):
             except AssertionError as e:
                 print(e)
 
-            mfo_field = driver.find_element(By.XPATH, "//input[@placeholder='Select MFO']")
-            mfo_field.clear()
-            mfo_field.send_keys(form_data["mfo"])
+            name_field = driver.find_element(By.XPATH, "//input[@id='is_name']")
+            name_field.clear()
+            name_field.send_keys(form_data["name"])
+
+            database_field = driver.find_element(By.XPATH, "//input[@placeholder='Select Database']")
+            database_field.clear()
+            database_field.send_keys(form_data["database"])
             time.sleep(1)
             options = WebDriverWait(driver, 10).until(
                 EC.visibility_of_all_elements_located((By.CLASS_NAME, "vs__dropdown-menu"))
@@ -117,21 +121,71 @@ def ProposedIS_add(driver):
             desired_option = options[0]  # Change the index as needed
             desired_option.click()
 
-            system_field = driver.find_element(By.XPATH, "//textarea[@id='critical_mngt']")
-            system_field.clear()
-            system_field.send_keys(form_data["system"])
+            description_field = driver.find_element(By.XPATH, "//textarea[@id='is_description']")
+            description_field.clear()
+            description_field.send_keys(form_data["description"])
 
-            problem_field = driver.find_element(By.XPATH, "//textarea[@id='problem']")
-            problem_field.clear()
-            problem_field.send_keys(form_data["problem"])
+            from_field = driver.find_element(By.XPATH, "//input[@placeholder='From']")
+            from_field.click()
+            WebDriverWait(driver, 10).until(
+                EC.visibility_of_all_elements_located((By.CLASS_NAME, "dp__menu"))
+            )
+            # Locate the year element (e.g., 1900) and click it
+            year_element = WebDriverWait(driver, 10).until(
+                EC.element_to_be_clickable((By.XPATH, "//div[@role='gridcell' and @data-test='2000']"))
+            )
+            year_element.click()
 
-            use_field = driver.find_element(By.XPATH, "//textarea[@id='intended']")
-            use_field.clear()
-            use_field.send_keys(form_data["use"])
+            # Locate the "Select" button and click it
+            select_button = WebDriverWait(driver, 10).until(
+                EC.element_to_be_clickable(
+                    (By.XPATH, "//button[@class='dp__action_button dp__action_select' and @data-test='select-button']"))
+            )
+            select_button.click()
+
+            to_field = driver.find_element(By.XPATH, "//input[@placeholder='To']")
+            to_field.click()
+            WebDriverWait(driver, 10).until(
+                EC.visibility_of_all_elements_located((By.CLASS_NAME, "dp__menu"))
+            )
+            # Locate the year element (e.g., 1900) and click it
+            year_element = WebDriverWait(driver, 10).until(
+                EC.element_to_be_clickable((By.XPATH, "//div[@role='gridcell' and @data-test='2000']"))
+            )
+            year_element.click()
+
+            # Locate the "Select" button and click it
+            select_button = WebDriverWait(driver, 10).until(
+                EC.element_to_be_clickable(
+                    (By.XPATH, "//button[@class='dp__action_button dp__action_select' and @data-test='select-button']"))
+            )
+            select_button.click()
+
+            status_field = Select(driver.find_element(By.XPATH, "//select[@id='is_status']"))
+            status_field.select_by_visible_text(form_data["status"])
+
+            strategy_field = Select(driver.find_element(By.XPATH, "//select[@id='is_dev_strat']"))
+            strategy_field.select_by_visible_text(form_data["strategy"])
+
+            scheme_field = driver.find_element(By.XPATH, "//input[@id='is_comp_scheme']")
+            scheme_field.clear()
+            scheme_field.send_keys(form_data["scheme"])
+
+            internal_field = driver.find_element(By.XPATH, "//input[@id='is_int_users']")
+            internal_field.clear()
+            internal_field.send_keys(form_data["internal"])
+
+            external_field = driver.find_element(By.XPATH, "//input[@id='is_ext_users']")
+            external_field.clear()
+            external_field.send_keys(form_data["external"])
+
+            owner_field = driver.find_element(By.XPATH, "//input[@id='is_sys_owner']")
+            owner_field.clear()
+            owner_field.send_keys(form_data["owner"])
 
             # Click the save button and handle confirmation
             save_button = WebDriverWait(driver, 10).until(
-                EC.visibility_of_element_located((By.XPATH, "/html[1]/body[1]/div[2]/div[2]/div[1]/div[2]/form[1]/div[6]/button[1]"))
+                EC.visibility_of_element_located((By.XPATH, "/html/body/div[2]/div[2]/div/div[2]/form/div[12]/button"))
             )
             assert save_button.is_displayed(), "Save button is not visible"
             assert save_button.text == "Save", "Save button text is incorrect"
@@ -144,7 +198,7 @@ def ProposedIS_add(driver):
             )
             # Check the actual text against expected text
             actual_text = success_message.text.strip()  # Strip whitespace for accurate comparison
-            expected_text = "Strategic Concern for ICT Use added successfully."
+            expected_text = "Proposed IS added successfully."
 
             if actual_text == expected_text:
                 # Find and click the OK button
