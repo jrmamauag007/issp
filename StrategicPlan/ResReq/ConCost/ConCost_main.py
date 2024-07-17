90212f03431e891b0662a7775652af9299d0b318
John Robert Mamauag <johnrobert.mamauag@g.msuiit.edu.ph>
2024-07-17 16:34:44 +0800

    Feat: Init ConCost test cases

---
+++ StrategicPlan/ResReq/ConCost/ConCost_main.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+import sys
+import os
+
+agency_path = 'C:/Users/jrmam/test/pythonProject/StrategicPlan'
+sys.path.insert(0, agency_path)
+
+from login import login
+from ConCost_add import ConCost_add
+
+def main():
+    # Initialize the browser driver
+    driver_path = r"C:\Users\jrmam\test\pythonProject\StrategicPlan\chromedriver.exe"
+    service = Service(driver_path)
+    driver = webdriver.Chrome(service=service)
+
+    try:
+        # Execute test cases
+        login(driver)
+        ConCost_add(driver)
+
+    finally:
+        # Close the browser
+        driver.quit()
+
+if __name__ == "__main__":
+    main()
